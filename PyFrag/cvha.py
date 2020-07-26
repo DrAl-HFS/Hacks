d8d4ef5317650b20b694de8f11196af3c7c97b95
DrAl-HFS <DrAl-HFS@github.com>
2020-07-26 15:53:16 +0100

    DIY hist not working

--- PyFrag/cvha.py
+++ PyFrag/cvha.py
@@ -41,18 +41,6 @@ def modelEM (bh,nc): # Not producing anything useful so far...
     em.trainEM(bh)
     print( em.getMeans() )
 
-if "__main__" == __name__ :
-    try:
-        imBGR8= cv2.imread(sys.argv[1]) #, cv2.IMREAD_GRAYSCALE)
-        print("->f32")
-        imBGR32= numpy.float32(imBGR8)
-        imHSV32= cv2.cvtColor(imBGR32, cv2.COLOR_BGR2HSV)
-        # NB: Conversion always generates H range 0.0, 360.0
-        imH32= imHSV32[:,:,0]
-        mm= ( numpy.min(imH32), numpy.max(imH32) )
-        print("H range:", mm)
-        imH8= numpy.uint8( imH32 * (0xFF / 360.0) )
-        info(imH8,None)
         #hist,bins= np.histogram(imH32)
         #for h,b in hist,bins:
         #    print(b,':',h)
@@ -70,10 +58,38 @@ if "__main__" == __name__ :
         #except: # cv2.error as e:
         #print("total:",nh,"(ex=",n-nh,")")
         #info(imHSV32[:,:,0],None)
+
+def myHist (dat,mm,nBins):
+    h= numpy.zeros( (nBins) )
+    print("myHist() -",dat.shape, mm, "->", h.shape)
+    step= (mm[1] - mm[0]) / (nBins - 1)
+    rs= 1.0 / s
+    print("- step=",step)
+    for d in dat:
+        i= int((d - mm[0]) * rs)
+        h[i]+= 1
+    return h, step
+
+if "__main__" == __name__ :
+    try:
+        imBGR8= cv2.imread(sys.argv[1]) #, cv2.IMREAD_GRAYSCALE)
+        #imHSV8= cv2.cvtColor(imBGR8, cv2.COLOR_BGR2HSV)
+        #bh= info(imHSV8,None)
+        #print("---")
+        print("->f32")
+        imBGR32= numpy.float32(imBGR8)
+        imHSV32= cv2.cvtColor(imBGR32, cv2.COLOR_BGR2HSV)
+        # NB: Conversion always generates H range 0.0, 360.0
+        imH32= imHSV32[:,:,0]
+        mm= ( numpy.min(imH32), numpy.max(imH32) )
+        print("H range:", mm)
+        fh,w= myHist(imH32, mm, 500)
+        print("w=",w)
+        dumpFH(fh,mm[0],w)
         print("---")
-        imHSV8= cv2.cvtColor(imBGR8, cv2.COLOR_BGR2HSV)
-        satMask= 0xFF * (imHSV8[:,:,1] > 0x1F).astype(numpy.uint8)
-        bh= info(imHSV8[:,:,0], satMask) # imHSV8[:,:,2])
+        imH8= numpy.uint8( imH32 * (0xFF / 360.0) )
+        satMask= 0xFF * (imH8 > 0x1F).astype(numpy.uint8)
+        bh= info(imH8, satMask)
         modelEM(bh,10)
     except:
         print("Usage:",sys.argv[0],"<image-file>")
