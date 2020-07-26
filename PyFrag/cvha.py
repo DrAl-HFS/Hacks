11633df861e49bc25b3a985b366efc053dfb9936
DrAl-HFS <DrAl-HFS@github.com>
2020-07-26 21:02:02 +0100

    Stupd bugs fixed

--- PyFrag/cvha.py
+++ PyFrag/cvha.py
@@ -41,23 +41,19 @@ def modelEM (bh,nc): # Not producing anything useful so far...
     em.trainEM(bh)
     print( em.getMeans() )
 
-        #hist,bins= np.histogram(imH32)
-        #for h,b in hist,bins:
-        #    print(b,':',h)
+def hist (dat,mm,nBins):
+    hist,binE= numpy.histogram(dat,bins=nBins,range=mm)
+    i= 0
+    for h,b in zip(hist,binE):
+        if h>0:
+            print(b,'[',i,']:',h)
+        i+= 1
+    return hist
         #try:
-        #    h= np.bincount(imH32,minlength=400)
-        #    print(h)
-        #except:
-        #    print("*ERR:", sys.exc_info()[0])
-        #sh= imH8.shape
-        #n= sh[0]*sh[1]
-        #print(sh,n)
-        #try:
-        #    xh= cv2.calcHist([imH8],[0],None,[1<<16],[0,1<<16])
-        #    nh= dumpBH(xh,0,1)
-        #except: # cv2.error as e:
-        #print("total:",nh,"(ex=",n-nh,")")
-        #info(imHSV32[:,:,0],None)
+    #hist= numpy.bincount(imH32,minlength=400)
+    #print(hist)
+    #try: except:
+    #    print("*ERR:", sys.exc_info()[0])
 
 def myHist (dat,mm,nBins):
     h= numpy.zeros( (nBins) )
@@ -73,23 +69,27 @@ def myHist (dat,mm,nBins):
 if "__main__" == __name__ :
     try:
         imBGR8= cv2.imread(sys.argv[1]) #, cv2.IMREAD_GRAYSCALE)
+    except:
+        print("Usage:",sys.argv[0],"<image-file>")
         #imHSV8= cv2.cvtColor(imBGR8, cv2.COLOR_BGR2HSV)
         #bh= info(imHSV8,None)
         #print("---")
-        print("->f32")
-        imBGR32= numpy.float32(imBGR8)
-        imHSV32= cv2.cvtColor(imBGR32, cv2.COLOR_BGR2HSV)
-        # NB: Conversion always generates H range 0.0, 360.0
-        imH32= imHSV32[:,:,0]
-        mm= ( numpy.min(imH32), numpy.max(imH32) )
+    print("->f32")
+    imBGR32= numpy.float32(imBGR8)
+    imHSV32= cv2.cvtColor(imBGR32, cv2.COLOR_BGR2HSV)
+    # NB: Conversion always generates H range 0.0, 360.0
+    imH32= imHSV32[:,:,0]
+    mm= ( numpy.min(imH32), numpy.max(imH32) )
+    h= hist(imH32.ravel(),mm,1000)
+    if False:
+        print("---")
         print("H range:", mm)
         fh,w= myHist(imH32, mm, 500)
         print("w=",w)
         dumpFH(fh,mm[0],w)
+    if False:
         print("---")
         imH8= numpy.uint8( imH32 * (0xFF / 360.0) )
         satMask= 0xFF * (imH8 > 0x1F).astype(numpy.uint8)
         bh= info(imH8, satMask)
-        modelEM(bh,10)
-    except:
-        print("Usage:",sys.argv[0],"<image-file>")
+    #modelEM(h,10)
