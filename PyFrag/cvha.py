73eee7e22d25ac46bce15bc9934fe7f48b16a6c6
DrAl-HFS <DrAl-HFS@github.com>
2020-07-27 10:04:55 +0100

    SciKit Learn tested, found wanting...

--- PyFrag/cvha.py
+++ PyFrag/cvha.py
@@ -5,6 +5,7 @@
 import sys
 import cv2
 import numpy
+from sklearn import mixture
 
 def dumpBH (bh):
     sum= 0
@@ -34,38 +35,32 @@ def info (img,msk,maxBin=256):
     print("total:",nh,"(ex=",n-nh,")")
     return bh
 
-def modelEM (bh,nc): # Not producing anything useful so far...
+def cvModelEM (h,nc): # Not producing anything useful so far...
     #em= cv2.ml.EM(bh, nclusters=nc) #, covMatType=EM::COV_MAT_DIAGONAL,TermCriteria
     em= cv2.ml.EM_create()
     em.setClustersNumber(nc)
-    em.trainEM(bh)
+    em.trainEM(h)
     print('M:', em.getMeans() )
     print('W:', em.getWeights() )
 
-def hist (dat,mm,nBins):
+def sklModelEM (h,nc):
+    em= mixture.GaussianMixture(n_components=nc, covariance_type='spherical') # Doesn't support 1D...
+    em.fit(h)
+    print(em)
+
+# No mask provision in this version
+def npHist (dat,mm,nBins,hvl=1000000000):
+    #hist= numpy.bincount(datI,minlength=nBins) # integral type only
     hist,binE= numpy.histogram(dat,bins=nBins,range=mm)
     i= 0
+    v= 0
     for h,b in zip(hist,binE):
-        if h>0:
+        if h>hvl:
             print(b,'[',i,']:',h)
+        v+= (h > 0)
         i+= 1
+    print("Occupancy:", ((100.0*v) / i),"%")
     return hist
-        #try:
-    #hist= numpy.bincount(imH32,minlength=400)
-    #print(hist)
-    #try: except:
-    #    print("*ERR:", sys.exc_info()[0])
-
-def myHist (dat,mm,nBins):
-    h= numpy.zeros( (nBins) )
-    print("myHist() -",dat.shape, mm, "->", h.shape)
-    step= (mm[1] - mm[0]) / (nBins - 1)
-    rs= 1.0 / s
-    print("- step=",step)
-    for d in dat:
-        i= int((d - mm[0]) * rs)
-        h[i]+= 1
-    return h, step
 
 if "__main__" == __name__ :
     try:
@@ -76,12 +71,17 @@ if "__main__" == __name__ :
         #bh= info(imHSV8,None)
         #print("---")
     print("->f32")
-    imBGR32= numpy.float32(imBGR8)
-    imHSV32= cv2.cvtColor(imBGR32, cv2.COLOR_BGR2HSV)
-    # NB: Conversion always generates H range 0.0, 360.0
+#    imBGR32= numpy.float32(imBGR8)
+    imHSV32= cv2.cvtColor(numpy.float32(imBGR8), cv2.COLOR_BGR2HSV)
+    # NB: Conversion always generates H range 0.0, 360.0 irrespective of input scale.
+    # (However S & V normed 0.0~1.0 as might be expected.)
+    #print("Sat32:", numpy.min(imHSV32[:,:,1]), numpy.max(imHSV32[:,:,1]) )
+    #satM8= 0xFF * (imHSV32[:,:,1] >= 0.125).astype(numpy.uint8)
     imH32= imHSV32[:,:,0]
+    sh= imH32.shape
+    nSh= sh[0] * sh[1]
     mm= ( numpy.min(imH32), numpy.max(imH32) )
-    h= hist(imH32.ravel(),mm,1000)
+    h= npHist(imH32.ravel(),mm,1000,nSh/100)
     if False:
         print("---")
         print("H range:", mm)
@@ -90,7 +90,10 @@ if "__main__" == __name__ :
         dumpFH(fh,mm[0],w)
     if False:
         print("---")
-        imH8= numpy.uint8( imH32 * (0xFF / 360.0) )
-        satMask= 0xFF * (imH8 > 0x1F).astype(numpy.uint8)
-        bh= info(imH8, satMask)
-    modelEM(h,10)
+        imHSV8= cv2.cvtColor(imBGR8, cv2.COLOR_BGR2HSV)
+        #imH8= numpy.uint8( imH32 * (0xFF / 360.0) ) # 0~0xFF
+        imH8= imHSV8[:,:,0] # 0 ~ 179
+        satM8= 0xFF * (imHSV8[:,:,1] > 0x1F).astype(numpy.uint8)
+        bh= info(imH8, satM8)
+    sklModelEM(h,10)
+    #cvModelEM(h,10)
