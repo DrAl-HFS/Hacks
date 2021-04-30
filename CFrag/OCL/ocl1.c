7b7c15429a97f8e8aae8d7c0041eabc7f2c6a8ee
DrAl-HFS <DrAl-HFS@github.com>
2021-04-30 16:30:18 +0100

    h

--- CFrag/OCL/ocl1.c
+++ CFrag/OCL/ocl1.c
@@ -10,6 +10,9 @@
 #define MAX_PF_ID    2
 #define MAX_DEV_ID   8
 
+const char *gAF[2]={"[%u]\n", "[%X]\n"};
+void printAlt (const char *af[], const int i, const size_t x) { printf(af[i], x); }
+
 int main (int argc, char *argv[])
 {
    cl_platform_id idPfrm[MAX_PF_ID]={0,};
@@ -22,18 +25,18 @@ int main (int argc, char *argv[])
    r= clGetPlatformIDs(MAX_PF_ID, idPfrm, &nPfrm);
    printf("DBG: clGetPlatformIDs() - %d [%u]\n", r, nPfrm);
 
-   for(int i = 0; i < nPfrm; i++)
+   for (int i = 0; i < nPfrm; i++)
    {
-      printf("DBG: id=0x%X\n", idPfrm[i]);
+      printf("DBG: id=0x%X\n", (size_t)idPfrm[i]);
       r= clGetDeviceIDs(idPfrm[i], CL_DEVICE_TYPE_ALL, MAX_DEV_ID, idDev, &nDev);
-      printf("DBG: clGetDeviceIDs( *ALL ) - %d [%u]\n", r, nDev);
+      printf("DBG: clGetDeviceIDs( *ALL ) - %d ", r); printAlt(gAF, nDev >= (1<<20), nDev);
       if (r < 0)
       {
          r= clGetDeviceIDs(idPfrm[i], CL_DEVICE_TYPE_CPU, MAX_DEV_ID, idDev, &nDev);
-         printf("DBG: clGetDeviceIDs( *CPU ) - %d [%u]\n", r, nDev);
+         printf("DBG: clGetDeviceIDs( *CPU ) - %d ", r); printAlt(gAF, nDev >= (1<<20), nDev);
 
          r= clGetDeviceIDs(idPfrm[i], CL_DEVICE_TYPE_GPU, MAX_DEV_ID, idDev, &nDev);
-         printf("DBG: clGetDeviceIDs( *GPU ) - %d [%u]\n", r, nDev);
+         printf("DBG: clGetDeviceIDs( *GPU ) - %d ", r);  printAlt(gAF, nDev >= (1<<20), nDev);
       }
    }
    return((nDev > 0)-1);
